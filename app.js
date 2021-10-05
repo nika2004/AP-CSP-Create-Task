9dbd4af5b86be89058b3332b2f9320631563990d
nika2004 <nika.imamberdieva22@sitechhs.com>
2021-10-04 21:26:33 -0400

    Changes 10/4/21

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 
-
 const currentDate = new Date();
-  let cDay = currentDate.getDate()
-  let cMonth = currentDate.getMonth() + 1
-  let cYear = currentDate.getFullYear()
+  let cDay = ("0" + currentDate.getDate()).slice(-2);
+  let cMonth = currentDate.getMonth() + 1;
+  let fMonth = ("0" + cMonth).slice(-2);
   console.log(cDay);
   console.log(cMonth);
-  console.log(cYear);
 
-  document.getElementById("today-is").innerHTML = `Today is ${cMonth}/${cDay}/${cYear}`;
-  console.log(`Today is ${cMonth}/${cDay}/${cYear}`); 
+const years = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 
+  2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021];
 
-  /* document.getElementById("search-submit").addEventListener("click", function () {
-    const input = document.getElementById("userInput").value;
-    console.log(input);
-    const userDate = input;
-    async function fetchAsync(){
-      let data = await (await fetch(`https://api.nasa.gov/planetary/apod?api_key=MiTfqF5OLo0PFUhLNYhRcjOttrsiDd7yrGZcLB0B&date=${userDate}`)).json();
+const random = [Math.floor(Math.random() * years.length)];
+console.log(random, years[[Math.floor(Math.random() * years.length)]]);
+
+function getRandomDate () {
+    async function fetchRandYear(){
+      let data = await (await fetch(`https://api.nasa.gov/planetary/apod?api_key=MiTfqF5OLo0PFUhLNYhRcjOttrsiDd7yrGZcLB0B&date=${years[[Math.floor(Math.random() * years.length)]]}-${cMonth}-${cDay}`)).json();
       console.log(data);
-      document.getElementById("date-is").innerHTML = data.date;
-      document.getElementById("date-title").innerHTML = data.title;
-      document.getElementById("date-image-div").innerHTML = `<img src="${data.hdurl}">`;
-      document.getElementById("date-info").innerHTML = data.explanation;
+      document.getElementById("today-is").innerHTML = `ON THIS DAY IN HISTORY <br id="break"> ${cMonth}/${cDay}/${years[[Math.floor(Math.random() * years.length)]]}`;
+      document.getElementById("today-title").innerHTML = data.title;
+      document.getElementById("today-image").innerHTML = `<img src="${data.hdurl}">`;
+      document.getElementById("today-info").innerHTML = data.explanation;
       }
-    fetchAsync()
+    fetchRandYear();
+  }
 
-  }); */
+document.getElementById("buttons-1").addEventListener("click", function() {getRandomDate() }); 
+
+
+
+
+
  
--- app2.js
+++ app2.js
@@ -5,11 +5,9 @@ document.getElementById("search-submit").addEventListener("click", function () {
     async function fetchAsync(){
       let data = await (await fetch(`https://api.nasa.gov/planetary/apod?api_key=MiTfqF5OLo0PFUhLNYhRcjOttrsiDd7yrGZcLB0B&date=${userDate}`)).json();
       console.log(data);
-      document.getElementById("date-is").innerHTML = data.date;
       document.getElementById("date-title").innerHTML = data.title;
-      document.getElementById("date-image-div").innerHTML = `<img src="${data.hdurl}">`;
+      document.getElementById("date-image").innerHTML = `<img src="${data.hdurl}">`;
       document.getElementById("date-info").innerHTML = data.explanation;
       }
     fetchAsync()
-
   });
